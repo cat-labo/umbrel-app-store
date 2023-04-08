af6f246ecc58a4da8c5b36c1b7f8924e282d6147
ikuradon <hawaiianphoto@geekhost.net>
2023-04-08 11:44:34 +0000

    [ligess] Fix env

--- cat-labo-ligess/exports.sh
+++ cat-labo-ligess/exports.sh
@@ -1,1 +1,2 @@
-export APP_LIGHTNING_MACAROON_INVOICES=$(xxd -ps -u -c 10000 ${APP_LIGHTNING_NODE_DATA_DIR}/data/chain/bitcoin/mainnet/invoices.macaroon)
+APP_DIR=${APP_BITCOIN_DATA_DIR}/../../../
+export APP_LIGHTNING_MACAROON_INVOICES=$(xxd -ps -u -c 10000 ${APP_DIR}/lightning/data/lnd/data/chain/bitcoin/mainnet/invoices.macaroon)
